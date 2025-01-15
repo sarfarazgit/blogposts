3b211f208cac782b80cc6442f7c3cbb8f0644be3
Sarfaraz Ahamad <saahamad@saahamad-thinkpadt14sgen1.rmtin.csb>
2025-01-15 19:26:33 +0530

    Adding test cases and code file for file system

---
+++ blogposts_test.go
@@ -0,0 +1,45 @@
+package blogposts_test
+
+import (
+	"reflect"
+	"testing"
+	"testing/fstest"
+
+	"github.com/sarfarazgit/blogposts"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1`
+		secondBody = `Title: Post 2
+Description: Description 2`
+	)
+
+	fs := fstest.MapFS{
+		"hello-world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+	// assertPost(t, posts[0], blogposts.Post{
+	// 	Title:       "Post 1",
+	// 	Description: "Description 1",
+	// })
+
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sarfarazgit/blogposts
+
+go 1.23.4
